7c286fbffb6f5dc5eae436d534b8a8f2a68af25f
José Luiz <jlz.dev@outlook.com>
2024-06-12 20:17:27 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// Adicione qualquer JavaScript necessário aqui
+// Neste exemplo, o JavaScript não é essencial para a funcionalidade básica
+
+AOS.init({
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    offset: 0, // offset (in px) from the original trigger point
+
+});
+
+
+function redirectLink(l){
+    
+    if(l == 1){
+        window.location.href = "#";
+    }
+
+}
