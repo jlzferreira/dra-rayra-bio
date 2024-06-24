fd7a8f0ef91d1cb450afe48f59301c155494ca90
José Luiz <jlz.dev@outlook.com>
2024-06-24 09:39:21 -0300

    ajusts

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@
 AOS.init({
     duration: 1000, // values from 0 to 3000, with step 50ms
     offset: 0, // offset (in px) from the original trigger point
+    once: true, // whether animation should happen only once - while scrolling down
 
 });
 
